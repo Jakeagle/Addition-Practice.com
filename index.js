d1c65cf9dd7f8be034c727e065082bd20356da03
Jakeagle <114176194+Jakeagle@users.noreply.github.com>
2023-10-30 15:41:51 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+var valueOne;
+var valueTwo;
+var result;
+var playerAnswer;
+
+
+function beginChallenge() {
+    var max = parseInt(document.getElementById("Max-Input").value);
+    var min = parseInt(document.getElementById("Min-Input").value);
+
+    valueOne = Math.floor(Math.random() * (max - min + 1)) + min;
+    valueTwo = Math.floor(Math.random() * (max - min + 1)) + min;
+    document.getElementById("valOne").innerHTML = valueOne;
+    document.getElementById("valTwo").innerHTML = valueTwo;
+    result = valueOne + valueTwo;
+    console.log(result);
+
+
+}
+
+function answerClick() {
+    if (event.key === "Enter") {
+        checkAnswer();
+    }
+}
+
+function resetValues() {
+    var min = "0";
+    var max = "0";
+    var valueOne = "0";
+    var valueTwo = "0";
+    var result = "0";
+    document.getElementById("valOne").innerHTML = "0 ";
+    document.getElementById("valTwo").innerHTML = "0 ";
+}
+
+function checkAnswer() {
+
+    playerAnswer = document.getElementById("answerValue").value;
+    console.log(playerAnswer);
+    if (playerAnswer == result) {
+        document.getElementById("answerValue").value = " ";
+        beginChallenge();
+    }
+    else if (playerAnswer != result) {
+        console.log("incorrect");
+        alert("Try Again");
+        document.getElementById("answerValue").value = " ";
+    }
+
+}
